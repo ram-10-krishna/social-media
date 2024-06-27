c4120d4f0cb9b2d5b8732c404674249c611bf491
Ram Krishna <ramk94159@gmail.com>
2024-06-27 19:34:57 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="container">
+      <footer className="py-3 my-4">
+        <p className="text-center text-body-secondary">© 2023 Company, Inc</p>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
